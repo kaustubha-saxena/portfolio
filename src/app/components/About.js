522c6f8387d3e90ff5841b23df0a01f40522fb83
Kaustubha Saxena <kaustubhasaxena@gmail.com>
2025-08-15 16:54:16 +0530

    updated

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
 <h2 style={{ WebkitTextStroke: "1px black" }} className='text-7xl font-[Poppins] font-bold text-transparent pb-10  selection:text-green-600  '>About me</h2>
     <div className='text-lg font-medium leading-10' >
 
-    I&apos;m Kaustubha Saxena, a B.Tech student at MIT Manipal, major in Information Technology, with a passion for web development and scalable technologies. Proficient in  <span className='text-green-600 font-semibold text-xl'>Java, JavaScript ,Python, React.js, Next.js, Node.js, and cloud services like AWS and Docker.</span> Always eager to expand my skills and tackle new challenges, I'm actively seeking lucrative opportunities to leverage my tech expertise and drive impactful projects. Whether it's through creating seamless web experiences or exploring the future of decentralized applications, I'm excited to contribute to the tech landscape and grow alongside it.
+    I&apos;m Kaustubha Saxena, a B.Tech student at MIT Manipal, major in Information Technology, with a passion for web development and scalable technologies. Proficient in  <span className='text-green-600 font-semibold text-xl'>Java, JavaScript ,Python, React.js, Next.js, Node.js, and cloud services like AWS and Docker.</span> Always eager to expand my skills and tackle new challenges, I&apos;m actively seeking lucrative opportunities to leverage my tech expertise and drive impactful projects. Whether it&apos;s through creating seamless web experiences or exploring the future of decentralized applications, I&apos;m excited to contribute to the tech landscape and grow alongside it.
     </div>
     </div>
     <div className='w-full flex justify-center items-center'>
